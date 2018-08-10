0ac33f6aa8de51f76ec0060a3a8dea601a3fb26a
sbrichardson <srichardson@reactual.io>
2018-08-10 20:31:35 +0800

    update

--- src/Cividis/cividisRGB.js
+++ src/Cividis/cividisRGB.js
@@ -3,9 +3,9 @@ import cividisRaw from './cividisRaw'
 export default cividisRaw.map(color => {
   const [dR, dG, dB] = color
 
-  const red = 255 * dR
-  const green = 255 * dG
-  const blue = 255 * dB
+  const red = 256 * dR
+  const green = 256 * dG
+  const blue = 256 * dB
 
   return { red, green, blue }
 })
